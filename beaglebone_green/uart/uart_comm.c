4c4f24b85e69ad7ce52621f9dc5080a23d19fb43
sandeeprajkumbargeri <sandeep.kumbargeri@colorado.edu>
2018-04-30 00:36:24 +0000

    Adding UART on BeagleBone

---
+++ beaglebone_green/uart/uart_comm.c
@@ -0,0 +1,107 @@
+/* Simple send and receive C example for communicating with the
+*  Arduino echo program using UART4. 
+*
+* Written by Derek Molloy for the book "Exploring BeagleBone: Tools and 
+* Techniques for Building with Embedded Linux" by John Wiley & Sons, 2014
+* ISBN 9781118935125. Please see the file README.md in the repository root 
+* directory for copyright and GNU GPLv3 license information.            */
+
+#include <stdio.h>
+#include <fcntl.h>  // File Control Definitions
+#include <termios.h>// POSIX Terminal Control Definitions*/
+#include <unistd.h> // UNIX Standard Definitions         */
+#include <errno.h>  
+#include <strings.h>
+#include "uart_comm.h"
+
+int UART4_Init()
+{
+  int fd;
+  struct termios SerialPortSettings;
+
+  fd = open("/dev/ttyO4",O_RDWR | O_NOCTTY);
+  if(fd == -1)
+  {
+     return fd;
+  }
+  else
+     printf("\n  ttyO4 Opened Successfully\n");
+
+  tcgetattr(fd, &SerialPortSettings);
+
+  cfsetispeed(&SerialPortSettings,B9600);
+  cfsetospeed(&SerialPortSettings,B9600);
+
+  SerialPortSettings.c_cflag &= ~PARENB;
+  SerialPortSettings.c_cflag &= ~CSTOPB;
+  SerialPortSettings.c_cflag &= ~CSIZE;
+  SerialPortSettings.c_cflag |=  CS8;
+  SerialPortSettings.c_cflag &= ~CRTSCTS;
+  SerialPortSettings.c_cflag |= CREAD | CLOCAL;
+  SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);
+  SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+  SerialPortSettings.c_cc[VMIN]  = 1; // Read 10 characters
+  SerialPortSettings.c_cc[VTIME] = 0;  // Wait indefinitely
+
+  tcsetattr(fd,TCSANOW,&SerialPortSettings);
+
+  return fd;
+}
+
+int UART4Send(int uart_fd, const uint8_t *write_buffer, uint32_t ui32Count)
+{
+  int bytes_written;
+
+  bytes_written = write(uart_fd,(void *)write_buffer, ui32Count);
+  if(bytes_written < 0)
+  {
+    return bytes_written;
+  }
+  return bytes_written;
+
+}
+int UART4Recv(int uart_fd, const uint8_t *read_buffer, uint32_t ui32Count)
+{
+  int bytes_read;
+
+  bytes_read = read(uart_fd, (void *)read_buffer, ui32Count);
+  if(bytes_read < 0)
+  {
+    return bytes_read;
+  }
+  return bytes_read;
+}
+
+void main()
+{
+  int fd;
+  char write_buffer[] = "ABCDEFGHIJKLMNO";
+  int  bytes_written  =  0;
+  fd = UART4_Init();
+  if(fd < 0)
+  {
+     printf("\nError in opening ttyO4(UART4)\n");
+     return;
+  }
+
+  bytes_written = UART4Send(fd, (uint8_t *)write_buffer, sizeof(write_buffer));
+  if(bytes_written < 0)
+  {
+    perror("Failed to write to Serial Port ttyO4(UART4)");
+  }
+  char buffer[100];
+  bzero(buffer, sizeof(buffer));
+
+  int bytes_read;
+
+  bytes_read = UART4Recv(fd, (uint8_t *)buffer, sizeof(buffer));
+
+  if(bytes_read < 0)
+  {
+    perror("Error reading from serial port");
+  } 
+
+  printf("\nReceived Message: %s\n", buffer);
+
+
+}
---
+++ beaglebone_green/uart/uart_comm.h
@@ -0,0 +1,23 @@
+/*
+ * uart_comm.h
+ *
+ *  Created on: Apr 28, 2018
+ *      Author: Prithvi Teja
+ */
+
+#ifndef UART_UART_COMM_H_
+#define UART_UART_COMM_H_
+
+#include <stdint.h>
+#include <termios.h>
+
+#define UART_PORT               0
+#define BAUD_RATE               B9600
+
+void UARTStdioIntHandler(void);
+void UART_Configure(uint32_t ui32SrcClock);
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count);
+
+
+#endif /* UART_UART_COMM_H_ */
+
